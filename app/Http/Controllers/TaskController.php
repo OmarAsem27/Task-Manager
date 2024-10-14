acd08c1ccfdbfccb30b14bdc3a3f7ef0c8fafd13
OmarAsem27 <omarasem6215@gmail.com>
2024-10-14 18:47:28 +0300

    added confirmation message for deleting task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -75,7 +75,7 @@ class TaskController extends Controller
      */
     public function update(UpdateTaskRequest $request, Task $task)
     {
-        //
+        return "edit";
     }
 
     /**
--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
 
     @vite('resources/css/app.css')
 
@@ -64,29 +66,34 @@
     </nav>
 
 
-    <div class="container text-center bg-gray-400 max-w-full ">
+    <div class="container text-center  max-w-full ">
 
-        <div class=" bg-gray-200 ">
+        <div class="  ">
             <h1 class="text-6xl"> Let's do some work</h1>
         </div>
 
         <div class="text-center bg-gray-200 ">
             @if ($tasks)
                 @foreach ($tasks as $task)
-                    <p> {{ $task->body }} -- {{ $task->status }} </p>
-                    <div class="flex justify-center">
-                        <form method="post" action="{{ route('tasks.destroy', $task) }}">
-                            @csrf
-                            @method('delete')
-                            <button type="submit"
-                                class="p-2 bg-red-700 hover:bg-red-800 rounded-xl text-white">Delete</button>
-                        </form>
-                        <form method="post" action="{{ route('tasks.update', $task) }}">
-                            @csrf
-                            @method('put')
-                            <button type="submit"
-                                class="p-2 bg-stone-700 hover:bg-stone-800 rounded-xl text-white">Edit</button>
-                        </form>
+                    <p> {{ $task->body }} -- {{ $task->status }} -- {{ $task->id }} </p>
+                    <div class="flex justify-center gap-x-6">
+                        <div>
+                            <button data-modal-target="{{ 'popup-modal' . $task->id }}"
+                                data-modal-toggle="{{ 'popup-modal' . $task->id }}"
+                                class="bg-red-700 hover:bg-red-800 text-white font-medium rounded-lg px-4 py-2">Delete</button>
+
+                        </div>
+                        <div>
+                            <form method="post" action="{{ route('tasks.update', $task) }}">
+                                @csrf
+                                @method('put')
+                                <button data-modal-target="default-modal" data-modal-toggle="default-modal"
+                                    class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                    type="submit">
+                                    Edit
+                                </button>
+                            </form>
+                        </div>
                     </div>
                     <p>
                         @foreach ($task->categories as $category)
@@ -94,6 +101,48 @@
                             <!-- Display category names separated by commas -->
                         @endforeach
                     </p>
+
+                    {{-- delete confirmation modal --}}
+                    <div id="{{ 'popup-modal' . $task->id }}" tabindex="-1"
+                        class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                        <div class="relative p-4 w-full max-w-md max-h-full">
+                            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                <button type="button"
+                                    class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                    data-modal-hide="{{ 'popup-modal' . $task->id }}">
+                                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                        fill="none" viewBox="0 0 14 14">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                    </svg>
+                                    <span class="sr-only">Close modal</span>
+                                </button>
+                                <div class="p-4 md:p-5 text-center">
+                                    <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
+                                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                        viewBox="0 0 20 20">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                    </svg>
+                                    <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure
+                                        you want to
+                                        delete this task?</h3>
+                                    <form method="post" action="{{ route('tasks.destroy', $task->id) }}">
+                                        @csrf
+                                        @method('delete')
+                                        <button data-modal-hide="{{ 'popup-modal' . $task->id }}" type="submit"
+                                            class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                                            Yes, I'm sure
+                                        </button>
+                                    </form>
+                                    <button data-modal-hide="{{ 'popup-modal' . $task->id }}" type="button"
+                                        class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No,
+                                        cancel</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 @endforeach
             @endif
         </div>
@@ -102,7 +151,7 @@
         <form class="max-w-sm mx-auto" method="POST" action="{{ route('tasks.store') }}">
             @csrf
             <div class="mb-5">
-                <label for="task" class="block mb-2 text-sm font-medium text-yellow-900 dark:text-red-500">Your
+                <label for="task" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
                     task</label>
                 <input type="text" id="task"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -110,7 +159,7 @@
             </div>
             <div class="mb-5">
                 <label for="category"
-                    class="block mb-2 text-sm font-medium text-yellow-900 dark:text-red-500">Category</label>
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
                 <input type="text" id="category"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="category..." name="category" required />
@@ -125,6 +174,8 @@
 
 
     </div>
+
+
 
 </body>
 
